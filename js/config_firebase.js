c3b931c8db69062621e00ed55c33273c55c49b90
Gabriel Freitas <freitasgabrield.o.a@gmail.com>
2023-10-23 20:27:36 -0300

    Criado projeto firebase

---
+++ js/config_firebase.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD-mQF02YgTE3PuOGRI2zfzpYjspS-aGu4",
+    authDomain: "projeto-tarefa-8c138.firebaseapp.com",
+    projectId: "projeto-tarefa-8c138",
+    storageBucket: "projeto-tarefa-8c138.appspot.com",
+    messagingSenderId: "559376804003",
+    appId: "1:559376804003:web:a5032eb518ed982e877716"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
